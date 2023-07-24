65b1b2b871b8545530715c626d447d74be543407
santhosh <96367822+santhoshmg160@users.noreply.github.com>
2023-07-24 14:24:06 +0530

    Update DbConnection.java

--- jdbc/src/DbConnection.java
+++ jdbc/src/DbConnection.java
@@ -2,10 +2,11 @@ import java.sql.*;
 
 public class DbConnection {
 
-    private static String url = "jdbc:mysql://localhost:3306/Db";
-    private static  String name ="root";
-    // Enter Your password
-    private static String password = "san123!@#SAN";
+    //Provide your database url and username and password;
+    private static String url = "";
+    private static  String name ="";
+
+    private static String password = "";
 
     static Connection getConnection() throws  SQLException {
         return DriverManager.getConnection(url, name, password);
